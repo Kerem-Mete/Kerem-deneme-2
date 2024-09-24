c3574f29bdb54cc7faf1843c71471fbfc92ccc0c
Kerem-Mete <krmslhmt@gmail.com>
2024-09-24 21:52:02 +0300

    Create main.py
    Bu github daki ilk kodum

---
+++ main.py
@@ -0,0 +1,19 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "GHOST": "Yok olmak",
+            "FAKE": "Sahte"
+            }
+
+
+
+
+
+
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("bu kelime sözlükte bulunmuyor")
